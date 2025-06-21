81de3b769023d973bd9dfd90dff1a60c2b495a34
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 18:25:59 +0530

    Create Loops.cpp

---
+++ Data Structures 1/Loops.cpp
@@ -0,0 +1,15 @@
+//Loops means continuation.
+//Loops means iteration if the condition satifies things starts executing.
+//Things executes until the condition satisfies.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int i;
+    for(int i = 1;i <=5; i = i + 1) {
+        cout << i << endl;
+    }
+    return 0;
+
+}
