a2d2bca4d4818168ca0b68737a43d454c702604d
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 18:45:33 +0530

    Create IfElse.cpp

---
+++ Basic C++/IfElse.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int age;
+    cin >> age;
+    if(age >= 18) {
+        cout << "You are an Adult!";
+    }
+    else {
+        cout << "You are not an Adult!";
+    }
+    return 0;
+}
