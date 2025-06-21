7c3dd5fd19061eee9945a80b7ef7859d8199d35a
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 18:18:37 +0530

    Update Header-template.cpp

--- Basic C++/Header-template.cpp
+++ Basic C++/Header-template.cpp
@@ -1,1 +1,16 @@
 // Describe the syntax of the code
+
+//      This program...
+//
+//      Other files required:
+//        1.	 example.h - header file for the example class
+//        2.	 example.txt - a text file containing the example data
+
+#include<iostream>
+using namespace std;
+int main() {
+  //Define the scope of code here.
+
+  //Shows the successful defining the scope of code.
+  return 0;
+}
