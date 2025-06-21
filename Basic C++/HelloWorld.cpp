c33df034a30f5183e946ce53399a6f97df55c235
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 18:16:50 +0530

    Commit HelloWorld.cpp

--- Basic C++/HelloWorld.cpp
+++ Basic C++/HelloWorld.cpp
@@ -1,1 +1,11 @@
 // Output of this code is Hello World
+
+#include<iostream>
+using namespace std;
+int main() {
+  //The scope of this code is described inside these curly brackets.
+  cout << "Hello World";
+
+  //This return 0 shows the successful execution of the scope of the code
+  return 0;
+}
