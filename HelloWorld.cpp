75ea413be1c81d756b5cdc0bd2704f98a93cfcdb
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 17:54:52 +0530

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+  //The scope of this code is described inside these curly brackets.
+  cout << "Hello World";
+
+  //This return 0 shows the successful execution of the scope of the code
+  return 0;
+}
