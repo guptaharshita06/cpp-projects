45edca93f15632cb00785b9458e2c5bc6586395d
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 18:34:07 +0530

    Create InputOutput.cpp

---
+++ Basic C++/InputOutput.cpp
@@ -0,0 +1,12 @@
+// For taking input we use cin.
+//For showing output we use cout.
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
+    return 0;
+}
