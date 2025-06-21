eaed480102a743ac44a71f5d7322360e68e582a3
guptaharshita06 <guptaharshitastar06@gmail.com>
2025-06-21 18:20:15 +0530

    Delete HelloWorld.cpp

--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-  //The scope of this code is described inside these curly brackets.
-  cout << "Hello World";
-
-  //This return 0 shows the successful execution of the scope of the code
-  return 0;
-}
