78886943c035215165165e6e48c40967b9f30022
Harshita Gupta <guptaharshitastar06@gmail.com>
2025-07-15 22:08:53 +0530

    Update doublepointer.cpp

--- Data Structures 2/doublepointer.cpp
+++ Data Structures 2/doublepointer.cpp
@@ -6,6 +6,13 @@ int main() {
 
     cout << "arr: " << arr << endl;
     cout << "&arr: " << &arr << endl;
+    //ptr-address of complete array
     cout << "ptr: " << ptr << endl;
+    //*ptr- address of starting of array block
     cout << "*ptr: " << *ptr << endl;
+    cout << "(*ptr)[0]: " << (*ptr)[0] << endl;
+    cout << "(*ptr)[1]: " << (*ptr)[1] << endl;
+    cout << "(*ptr)[2]: " << (*ptr)[2] << endl;
+    cout << "arr+1: " << arr+1 << endl;
+    cout << "ptr+1: " << ptr+1 << endl;
 }
